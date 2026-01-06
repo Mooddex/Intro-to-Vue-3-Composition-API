33b703d59be356c3d9ab0aa983b34479afdea5b0
Mahmoud <msalama027@gmail.com>
2026-01-06 18:22:49 +0200

    lists done

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-const inStock = true
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+const inStock = true;
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+const variants = ref([
+  { id: 1234, color: 'green' },
+  { id: 5678, color: 'blue' },
+]);
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div v-for="variante in variants" :key="variante.id">{{ variante.color }}</div>
       </div>
     </div>
   </div>
